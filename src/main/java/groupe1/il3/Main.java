7963813734470ee2748c6a25062d0c34476e3f9d
Nathan Ferry <nathan.ferry@live.fr>
2026-01-06 09:58:52 +0100

    INIT

---
+++ src/main/java/groupe1/il3/Main.java
@@ -0,0 +1,12 @@
+package groupe1.il3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        System.out.println("Hello world!");
+    }
+}
