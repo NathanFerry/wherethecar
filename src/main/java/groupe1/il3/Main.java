dd3c52fb640b21f3220c5627d6131c995ad6f45d
Nathan FERRY <nathan.ferry@live.fr>
2026-01-08 10:35:34 +0100

    Test sample

--- src/main/java/groupe1/il3/Main.java
+++ src/main/java/groupe1/il3/Main.java
@@ -1,12 +1,25 @@
 package groupe1.il3;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        System.out.println("Hello world!");
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Label label = new Label("Hello, World!");
+
+        Scene root = new Scene(label, 400, 200);
+
+        primaryStage.setScene(root);
+        primaryStage.setTitle("Hello World");
+        primaryStage.show();
     }
 }
