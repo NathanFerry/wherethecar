1fd33531368fc9d454cd85a16b70f171cce88048
Nathan FERRY <nathan.ferry@live.fr>
2026-01-08 13:40:05 +0100

    Renamed

--- src/main/java/groupe1/il3/App.java
+++ src/main/java/groupe1/il3/App.java
@@ -7,10 +7,7 @@ import javafx.stage.Stage;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main extends Application{
-    public static void main(String[] args) {
-        launch(args);
-    }
+public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
