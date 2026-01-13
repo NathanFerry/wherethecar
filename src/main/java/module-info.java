c2efd8b6186c5549970058a7c4f6e511eff20f43
Nathan FERRY <nathan.ferry@live.fr>
2026-01-13 15:45:07 +0100

    adding postgre jdbc dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module wherethecar.main {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.fxml;
+    requires org.postgresql.jdbc;
 
     opens groupe1.il3.app to javafx.graphics, javafx.fxml;
     exports groupe1.il3.app;
