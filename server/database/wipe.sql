9a06209072add2af9ef7c7970fcb7e84c66a8e32
Nathan FERRY <nathan.ferry@live.fr>
2026-01-14 10:45:35 +0100

    Database creation and deletion scripts

---
+++ server/database/wipe.sql
@@ -0,0 +1,10 @@
+drop table if exists picture;
+drop table if exists maintenance_document;
+drop table if exists maintenance_operation;
+drop table if exists reservation;
+drop table if exists vehicle;
+drop table if exists agent;
+
+drop type if exists energy;
+drop type if exists vehicle_status;
+drop type if exists reservation_status;
