888f3cd583d7be7b7cb7569b1478f30ed029ad1d
Nathan FERRY <nathan.ferry@live.fr>
2026-01-23 12:09:24 +0100

    Removed unused feature

--- src/main/java/groupe1/il3/framework/di/provider/ServiceProvider.java
+++
@@ -1,7 +0,0 @@
-package groupe1.il3.framework.di.provider;
-
-import groupe1.il3.framework.di.container.DependencyInjectionContainer;
-
-public interface ServiceProvider {
-    void register(DependencyInjectionContainer container);
-}
